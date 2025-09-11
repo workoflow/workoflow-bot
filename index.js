84c5ad259b6d9218eeb7b6124d80ed242e088595
Patrick Schönfeld <patrickjajaa@gmail.com>
2025-09-11 20:05:26 +0200

    oai api proxy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const dotenv = require('dotenv');
 // Import required bot configuration.
 const ENV_FILE = path.join(__dirname, '.env');
-dotenv.config({ path: ENV_FILE, override: false });
+dotenv.config({ path: ENV_FILE });
 
 const restify = require('restify');
 
